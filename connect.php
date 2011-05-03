9c7887f6425aa9966b9d85585367628a9554beb4
unknown <Lidka@.(none)>
2011-05-04 00:41:19 +0300

    sait

--- connect.php
+++ connect.php
@@ -1,6 +1,6 @@
 <?php
   session_start();
-  $resource=mysql_connect("localhost","root","lida900128") or die("Could not connect".mysql_error());
+  $resource=mysql_connect("localhost","root","") or die("Could not connect".mysql_error());
   mysql_query('set names utf8', $resource);
   mysql_select_db("love.net",$resource) or die("Could not find database".mysql_error());
 ?> 
